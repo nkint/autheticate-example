401877f186b7385dad1e3c9dbbf24942ff9440b5
nkint <nootropic.kint@gmail.com>
2014-07-07 14:43:21 +0200

    installation script

---
+++ install.sh
@@ -0,0 +1,20 @@
+echo "Installing appA"
+cd appA
+npm install --quiet 
+cd ..
+
+echo "Installing appB"
+cd appB
+npm install --quiet 
+cd ..
+
+echo "Create the DB"
+cd appA
+cd db
+rm users.db
+echo ".read users.sql" | sqlite3 users.db
+cd ../..
+
+echo "Now test.."
+cd test
+./all_tests.sh
